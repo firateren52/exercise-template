77a4ce51afd790ec18172295393f31f4c3c74c09
Fırat Faruk Eren <fe@getzuper.com>
2019-12-10 04:47:33 +0300

    Add serverless-offline and run project on local

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import * as routes from "./handlers/handlers";
 import * as response from "./lib/http-responses";
 
@@ -13,15 +12,29 @@ Object.keys(routes).forEach(key => {
     return item;
 });
 export const echo = function (context, req) {
-  context.log(req);
-  let resourcePath = req.params.action;
-  if (resourcePath in handlers) {
-      handlers[resourcePath](req, context, responseCallback);
-  } else {
-      responseCallback(response.ERROR_404);
-  }
+    const request = setRequestForLocal(context, req);
+    console.log(`request: ${JSON.stringify(request)}`);
 
-  function responseCallback(response) {
-      context.done(null, { body: JSON.stringify(response), status: response.code });
-  }
+    let resourcePath = request.params.action;
+    if (resourcePath in handlers) {
+        handlers[resourcePath](request, context, responseCallback);
+    } else {
+        responseCallback(response.ERROR_404);
+    }
+
+    function responseCallback(response) {
+        const responseBody = {body: JSON.stringify(response), status: response.code};
+        console.log(`response: ${JSON.stringify(responseBody)}`);
+        request.done(null, responseBody);
+    }
+
+    function setRequestForLocal(context, req): any {
+        // request object is needed for locally run azure serverless functions
+        return {
+            method: req.method || context.httpMethod,
+            query: req.query || context.queryStringParameters,
+            params: req.params || context.pathParameters,
+            done: req.done || context.done,
+        };
+    }
 };
